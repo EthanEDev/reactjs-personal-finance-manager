9d567481c0a6f4fc39482af57b1d6fb7397d4e9f
EthanEDev <ethan.ekhtiyar@gmail.com>
2025-01-28 00:30:39 +0330

    Initialize the project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}', // Adjust this path to match your project's file structure
+    './public/index.html',       // Include HTML files if applicable
+  ],
+  darkMode: 'class', // Enables dark mode using the 'class' strategy
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
